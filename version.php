30b3105bc78a94e2922a5aceeb294a8dbae3375d
RED <support@ned.ca>
2024-09-20 13:29:23 -0400

    Update version.php

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2023051700;
+$plugin->version   = 2023051800;
 $plugin->requires  = 2021051700;
 $plugin->component = 'qtype_recordrtc';
 $plugin->maturity  = MATURITY_STABLE;
